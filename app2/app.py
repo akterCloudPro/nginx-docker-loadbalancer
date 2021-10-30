d5f99ad71f0ccab2bc5d228aebdd6b988440446f
Akter Hossain <getakter@gmail.com>
2021-10-30 13:51:52 +0000

    added docker-compose file and modified app, nginx files

--- app2/app.py
+++ app2/app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, from APP-02!'
+    return 'Hello, from APP-2!'
--- app3/app.py
+++ app3/app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, from APP-1!'
+    return 'Hello, from APP-3!'
