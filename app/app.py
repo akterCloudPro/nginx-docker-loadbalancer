32767e900543f459d1247920c58f24e81c125ed4
Akter Hossain <getakter@gmail.com>
2021-11-06 07:19:47 +0000

    updated codes

--- app/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-import socket
-container_id = socket.gethostname()
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, from akterCloudPro - [CID]' + container_id 
